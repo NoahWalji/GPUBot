62afade83fa34f5a96be23a48ef260a8e2ec6717
NoahWalji <noah@socastdigital.com>
2020-12-05 16:40:54 -0500

    Fixed headers

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def search(query):
     currentPage = []
 
     ## Header for web scrapping TODO: (Change not sure if needed?)
-     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
     ## Main For Loop: Loops through all URLS and scrapes each URL with the selected item
     i = 0;
